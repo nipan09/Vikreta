8b07fc7c84e0e7408d15e14857f6bd4f77cd5ae7
nipan09 <pnishant46@gmail.com>
2020-03-20 16:16:59 +0530

    product quantity in cart

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from products.models import Product
 from cart.models import Cart
 
 def add_to_cart(request, slug):
-	item = get_object_or_404(Product,slug=slug)
-	order_item, created = Cart.objects.get_or_create(
+	#with Cart model
+	item = get_object_or_404(Product,slug=slug)  #product name on the basis of slug
+	order_item, created = Cart.objects.get_or_create(     #order_item <-- (product name in cart)
 		item=item,
 		user=request.user
 	)
-	return redirect("mainapp:home")
+	#with Order model
+	order_qs = Order.objects.filter(user=request.user, ordered=False)  #order_qs <-- username for the unordered items
+	if order_qs.exists():
+		uzer = order_qs[0]
+		if uzer.orderitems.filter(item__slug=item.slug).exists():
+			order_item.qunatity+=1
+			messages.info(request, "This item quantity is updated")
+		else:
+			uzer.orderitems.add(order_item)
+			messages.info(request,"This item is added to your cart")
+	else:
+		Order.objects.create(user=request.user).add(orderitem=order_item)
+		messages.info(request, "This item is added to your cart")
+	return redirect ("mainapp:home")
+
 
 def remove_from_cart(request, slug):
 	item = get_object_or_404(Product,slug=slug)
@@ -18,3 +34,4 @@ def remove_from_cart(request, slug):
 	else:
 		messages.info("The iten was not in your cart")
 	return redirect("mainapp:home")
+	
